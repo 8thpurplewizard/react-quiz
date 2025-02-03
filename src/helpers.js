d6948a5e34c0423c668a711e5cb6470e42d3adbe
Georgios Potsios <gpotsios@deloitte.gr>
2025-02-03 22:14:44 +0200

    Showing answers suffled implementation

---
+++ src/helpers.js
@@ -0,0 +1,14 @@
+export const shuffleAnswers = question => {
+    const unsuffledAnswers = [
+       question.correctAnswer,
+       ...question.incorrectAnswers 
+    ]
+
+    return unsuffledAnswers
+                    .map(unsuffledAnswer => ({
+                    sort: Math.random(),
+                    value: unsuffledAnswer,
+                }))
+                .sort((a,b) => a.sort - b.sort)
+                .map((a) => a.value);
+}
